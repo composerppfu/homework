7be964ae7d6cae00ea8962f7b63362249eb3babb
qweasd98710 <qweasd98710@gmail.com>
2021-02-04 15:25:45 +0800

    studen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+// constant
+import { studentList} from './student.const';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog';
+  list = studentList;
 }
